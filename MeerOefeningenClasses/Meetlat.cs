8c1208851c5a1828c8c45d1bbc91ddebd42a9ecb
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-03-17 13:19:03 +0100

    Add project files.

---
+++ MeerOefeningenClasses/Meetlat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeerOefeningenClasses
+{
+    class Meetlat
+    {
+        public double LengteInM { get; private set; }
+        public double LengteInCm { get; private set; }
+        public double LengteInKm { get; private set; }
+        public double LengteInVoet { get; private set; }
+
+        private double beginLengte;
+        public Meetlat(double beginLengte)
+        {
+            BeginLengte = beginLengte;
+        }
+        public double BeginLengte
+        {
+            private get { return beginLengte; }
+            set 
+            { 
+                beginLengte = value;
+                LengteInM = value;
+                LengteInCm = value * 100;
+                LengteInKm = value / 1000;
+                LengteInVoet = value * 3.2808;          
+            
+            }
+        }
+        public void ShowMeetlat() 
+        {
+
+            Console.WriteLine($"lengte in cm = {LengteInCm}");
+            Console.WriteLine($"lengte in m = {LengteInM}");
+            Console.WriteLine($"lengte in km = {LengteInKm}");
+            Console.WriteLine($"lengte in voet = {LengteInVoet}");
+        }
+
+    }
+}
---
+++ MeerOefeningenClasses/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeerOefeningenClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Oefening Pokemon
+            Random random = new Random();
+            Pokemon bulbasaur = new Pokemon(50,40,40,55,55,20,1,"Bulbasaur","Grass");
+            Pokemon pickachu = new Pokemon(30, 40, 40, 55, 59, 40, 35, "Pickachu", "Electric");
+            List<Pokemon> pokedex = new List<Pokemon>();
+            pokedex.Add(Maakpokemon(random));
+            //bulbasaur.ShowInfo();
+            //pickachu.ShowInfo();
+            pokedex.Add(Maakpokemon(random));
+           
+            Pokemon pokemon = null;
+            Console.WriteLine(BattleTester(pickachu, bulbasaur));
+            
+            //Oefening Meetlat
+            
+            Meetlat meetlat = new Meetlat(2);
+            
+            meetlat.ShowMeetlat();
+            Console.ReadLine();
+        }       
+        static Pokemon Maakpokemon(Random random) 
+        {
+            int tempstat1 = random.Next(1, 20);
+            int tempstat2 = random.Next(1, 20);
+            int tempstat3 = random.Next(1, 20);
+            int tempstat4 = random.Next(1, 20);
+            int tempstat5 = random.Next(1, 20);
+            int tempstat6 = random.Next(1, 20);
+            Pokemon pokemon = new Pokemon(tempstat1, tempstat2, tempstat3, tempstat4, tempstat5, tempstat6, 404, "RandomGen", "???");
+            return pokemon;
+        }
+        static int BattleTester(Pokemon pokemon1,Pokemon pokemon2) 
+        {
+            if (pokemon2 == null && pokemon1 == null)
+            {
+                return 0;
+            }
+            else if (pokemon2 == null)
+            {
+                return 1;
+            }
+            else if (pokemon1 == null)
+            {
+                return 2;
+            }
+
+            if (pokemon1.Average() > pokemon2.Average())
+            {
+                return 1;
+            }
+            else if (pokemon1.Average() == pokemon2.Average())
+            {
+                return 0;
+            }
+            else
+            {
+                return 2;
+            }
+
+        }
+
+    }
+}
